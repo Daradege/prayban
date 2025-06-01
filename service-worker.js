45f99314b798f8527f748a071f26dfd07d905bb2
Daradege <darg.q.a.a@gmail.com>
2025-06-01 15:15:49 +0330

    feat: Improve service worker caching strategy and offline support
    This commit enhances the service worker's caching strategy and offline support capabilities.
    
    - Adds `skipWaiting()` and `clients.claim()` to the activate event to ensure the newest version of the service worker is used immediately.
    - Updates the fetch event listener to prioritize cached responses and fall back to the network.
    - Caches network responses after fetching and serves them for subsequent requests.
    - Handles navigation requests in offline mode by serving the `/index.html` from the cache.
    - Removes unnecessary fetch logic.

--- service-worker.js
+++ service-worker.js
@@ -11,6 +11,7 @@ const urlsToCache = [
   '/rakatshomar/index.html',
   '/taghvim/index.html',
   '/zekr/index.html',
+  
   'https://cdn.jsdelivr.net/gh/Daradege/prayban@main/node_modules/moment/min/moment.min.js',
   'https://cdn.jsdelivr.net/gh/Daradege/prayban@main/node_modules/moment-jalaali/build/moment-jalaali.js',
   'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css'
@@ -21,16 +22,40 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('/index.html');
+      }
+    })
   );
 });
